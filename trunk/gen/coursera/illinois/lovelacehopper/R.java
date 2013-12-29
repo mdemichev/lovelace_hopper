6f2ce6a67b045f86d575dec18f37f87b5a16f63b
mdemichev <mdemv@ya.ru>
2013-12-29 22:01:10 +0300

    Написана активити для Ады Лавлейс ( вертикальная ориентация )

--- trunk/gen/coursera/illinois/lovelacehopper/R.java
+++ trunk/gen/coursera/illinois/lovelacehopper/R.java
@@ -22,9 +22,16 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int lovelace_portrait=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int AdaDescription=0x7f080004;
+        public static final int AdaName=0x7f080000;
+        public static final int AdaPortrait=0x7f080001;
+        public static final int action_settings=0x7f080006;
+        public static final int scrollView1=0x7f080002;
+        public static final int textView1=0x7f080003;
+        public static final int textView2=0x7f080005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -34,8 +41,10 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
+        public static final int ada_description=0x7f050004;
+        public static final int ada_name=0x7f050002;
+        public static final int ada_portrait_desc=0x7f050003;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
     }
     public static final class style {
         /** 
